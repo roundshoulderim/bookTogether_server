55624262d1389d39c992c95064771ce566badd43
Andrew Chung <drew.c3071@gmail.com>
2019-11-16 04:35:01 +0900

    define userservice functions for acessing db on login/signup

---
+++ src/services/userService.ts
@@ -0,0 +1,43 @@
+import User from "../models/user";
+import bcrypt from "bcrypt";
+
+interface IUserInfo {
+  email: string;
+  name: string;
+  password: string;
+}
+
+const UserService: any = {
+  signUp: async (userInfo: IUserInfo): Promise<object> => {
+    const existing = await User.findOne({ email: userInfo.email });
+    if (existing) {
+      return {
+        error: {
+          status: 409,
+          type: "DuplicateEmail",
+          message: "이미 사용중인 이메일 주소입니다."
+        }
+      };
+    }
+    const hashedPassword: string = await bcrypt.hash(userInfo.password, 10);
+    const user: any = new User({ ...userInfo, password: hashedPassword });
+    await user.save();
+    return { message: "성공적으로 가입 되었습니다." };
+  },
+
+  login: async (userInfo: IUserInfo): Promise<object> => {
+    const { email, password } = userInfo;
+    const user: any = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return {
+        error: {
+          status: 400,
+          type: "LoginFailed",
+          message: "입력하신 이메일과 비밀번호가 일치하지 않습니다."
+        }
+      };
+    }
+    return { id: user.id, message: "성공적으로 로그인 되었습니다." };
+  }
+};
+export default UserService;
