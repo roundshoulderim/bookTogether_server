3fb9b33d366376f57c875c8ccada944e5c9d7f98
Andrew Chung <drew.c3071@gmail.com>
2019-11-17 05:22:37 +0900

    add review router

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import authRouter from "./auth";
+import reviewRouter from "./review";
 const AppRouter: express.Router = express.Router();
 
 AppRouter.use("/auth", authRouter);
+AppRouter.use("/reviews", reviewRouter);
 
 export default AppRouter;
