ed2b0bc31170285c1e7f94d99e8889c1685a7dde
Andrew Chung <drew.c3071@gmail.com>
2019-11-16 10:38:30 +0900

    add required fields to schema / change name casing

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 const Schema: any = mongoose.Schema;
 
 const UserSchema: mongoose.Schema = new Schema({
-  email: String, // email must be unique
-  name: String,
-  password: String
+  email: { type: String, required: true }, // email must be unique
+  name: { type: String, required: true },
+  password: { type: String, required: true }
 });
 
 const User: any = mongoose.model("user", UserSchema);
