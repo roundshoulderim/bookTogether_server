ce833d8a2567ef18d9d16f35527e796e35938d74
Andrew Chung <drew.c3071@gmail.com>
2019-11-19 10:37:41 +0900

    add routing for /user

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -4,6 +4,7 @@ import bookRouter from "./book";
 import curationRouter from "./curation";
 import ratingRouter from "./rating";
 import reviewRouter from "./review";
+import userRouter from "./user";
 const AppRouter: express.Router = express.Router();
 
 AppRouter.use("/auth", authRouter);
@@ -11,5 +12,6 @@ AppRouter.use("/books", bookRouter);
 AppRouter.use("/curations", curationRouter);
 AppRouter.use("/ratings", ratingRouter);
 AppRouter.use("/reviews", reviewRouter);
+AppRouter.use("/user", userRouter);
 
 export default AppRouter;
