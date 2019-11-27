be3b7724a4f3fb6d546eb8e8b0132f088df29e74
Andrew Chung <drew.c3071@gmail.com>
2019-11-27 09:28:38 +0900

    auto-index books on save & add search functionality

--- src/models/Book.ts
+++ src/models/Book.ts
@@ -1,8 +1,22 @@
-import mongoose from "mongoose";
-import mongoosePaginate from "mongoose-paginate-v2";
+import mongoose, { HookNextFunction } from "mongoose";
+import client from "../config/elasticsearch";
+import esIndex from "../helpers/middleware/esIndex";
 const Schema: any = mongoose.Schema;
 
-// Required fields: will be denormalized and referenced directly by other collections
+interface ISearchQuery {
+  query: string;
+  page: number;
+  size: number;
+}
+
+interface ISearchResults {
+  books: object[];
+  results_count: number;
+  pageable_count: number;
+  current_page: number;
+  is_end: boolean;
+}
+
 export const BookSchema: mongoose.Schema = new Schema(
   {
     authors: [String],
@@ -20,7 +34,41 @@ export const BookSchema: mongoose.Schema = new Schema(
   { versionKey: false }
 );
 
-BookSchema.plugin(mongoosePaginate); // add pagination method
+// Save updated index in ElasticSearch before updating MongoDB
+BookSchema.pre("save", async function(next: HookNextFunction): Promise<void> {
+  await esIndex(this);
+  next();
+});
 
-const Book: mongoose.PaginateModel<any> = mongoose.model("Book", BookSchema);
+// Return paginated search results from ElasticSearch
+BookSchema.statics.search = async ({ query, page, size }: ISearchQuery) => {
+  const results: ISearchResults = {} as ISearchResults;
+  const response = await client.search({
+    index: "books",
+    from: (page - 1) * size,
+    size,
+    sort: "_score",
+    body: {
+      query: {
+        multi_match: {
+          query,
+          analyzer: "standard",
+          fields: ["authors", "title^2"]
+        }
+      }
+    }
+  });
+  results.books = response.body.hits.hits.map((result: any) => {
+    result._source._id = result._id;
+    return result._source;
+  });
+  results.results_count = response.body.hits.total.value;
+  results.pageable_count = Math.ceil(results.results_count / size);
+  results.current_page = page;
+  results.is_end = page >= results.pageable_count;
+  return results;
+};
+
+const Book: any = mongoose.model("Book", BookSchema);
+
 export default Book;
