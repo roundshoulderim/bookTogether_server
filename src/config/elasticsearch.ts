668387a0970e27446c4e17f17c1be0ec7e501c50
Andrew Chung <drew.c3071@gmail.com>
2019-11-28 06:51:38 +0900

    config for /reviews index in elasticsearch and migration

--- src/config/elasticsearch.ts
+++ src/config/elasticsearch.ts
@@ -1,7 +1,9 @@
 import { Client } from "@elastic/elasticsearch";
 import migrateBooks from "./esmigration/migrateBooks";
+import migrateReviews from "./esmigration/migrateReviews";
 import dotenv from "dotenv";
 dotenv.config();
+
 const client: Client = new Client({
   cloud: {
     id: process.env.ES_CLOUD_ID,
@@ -10,25 +12,27 @@ const client: Client = new Client({
   }
 });
 
-const body = {
-  settings: {
-    analysis: {
-      filter: {
-        autocomplete_filter: {
-          type: "edge_ngram",
-          min_gram: 1,
-          max_gram: 40
-        }
-      },
-      analyzer: {
-        autocomplete: {
-          filter: ["lowercase", "autocomplete_filter"],
-          type: "custom",
-          tokenizer: "whitespace"
-        }
+const settings = {
+  analysis: {
+    filter: {
+      autocomplete_filter: {
+        type: "edge_ngram",
+        min_gram: 1,
+        max_gram: 40
+      }
+    },
+    analyzer: {
+      autocomplete: {
+        filter: ["lowercase", "autocomplete_filter"],
+        type: "custom",
+        tokenizer: "whitespace"
       }
     }
-  },
+  }
+};
+
+const booksBody = {
+  settings,
   mappings: {
     properties: {
       authors: {
@@ -43,13 +47,44 @@ const body = {
   }
 };
 
+const reviewsBody = {
+  settings,
+  mappings: {
+    properties: {
+      author: {
+        type: "nested",
+        properties: {
+          name: { type: "text", analyzer: "autocomplete" }
+        }
+      },
+      books: {
+        type: "nested",
+        properties: {
+          authors: { type: "text", analyzer: "autocomplete" },
+          title: { type: "text", analyzer: "autocomplete" }
+        }
+      },
+      title: {
+        type: "text",
+        analyzer: "autocomplete"
+      }
+    }
+  }
+};
+
 (async (): Promise<void> => {
   try {
-    // await client.indices.delete({ index: "books" }); // For restarting ES
-    const existing = await client.indices.exists({ index: "books" });
-    if (!existing.body) {
-      await client.indices.create({ index: "books", body });
+    // await client.indices.delete({ index: "books" });
+    // await client.indices.delete({ index: "reviews" });
+    const booksIndex = await client.indices.exists({ index: "books" });
+    const reviewsIndex = await client.indices.exists({ index: "reviews" });
+    if (!booksIndex.body) {
+      await client.indices.create({ index: "books", body: booksBody });
       migrateBooks();
+    }
+    if (!reviewsIndex.body) {
+      await client.indices.create({ index: "reviews", body: reviewsBody });
+      migrateReviews();
     }
   } catch (error) {
     console.log(error);
