ec780e0db6e9ca7e705e367e682c6ba7f73d3c97
Andrew Chung <drew.c3071@gmail.com>
2019-11-29 15:06:27 +0900

    complete getUser with resetPasswordToken query

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,19 +1,47 @@
 import User from "../models/User";
 
+interface IQuery {
+  resetPasswordToken: string;
+  user: string;
+}
+
 const userService = {
-  getUser: async (id: string) => {
-    return await User.findById(id).populate({
-      path: "to_read.book reading.book finished.book",
-      select: "authors thumbnail title"
-    });
+  getUser: async ({ resetPasswordToken, user }: IQuery) => {
+    if (resetPasswordToken) {
+      const userDoc: any = await User.findOne({ resetPasswordToken })
+        .populate({
+          path: "to_read.book reading.book finished.book",
+          select: "authors thumbnail title"
+        })
+        .select("-password");
+      if (!userDoc) {
+        const message: string = "잘못된 비밀번호 재설정 토큰입니다.";
+        return Promise.reject({ status: 404, type: "InvalidToken", message });
+      } else if (Date.now() >= userDoc.resetPasswordExpires) {
+        const message: string =
+          "만료된 토큰입니다. 비밀번호 재설정 요청을 다시 해주세요.";
+        return Promise.reject({ status: 403, type: "ExpiredToken", message });
+      } else {
+        return userDoc;
+      }
+    } else {
+      return await User.findById(user)
+        .populate({
+          path: "to_read.book reading.book finished.book",
+          select: "authors thumbnail title"
+        })
+        .select("-password");
+    }
   },
 
   patchUser: async (id: string, body: object) => {
     await User.findByIdAndUpdate(id, body);
-    return await User.findById(id).populate({
-      path: "to_read.book reading.book finished.book",
-      select: "authors thumbnail title"
-    });
+    return await User.findById(id)
+      .populate({
+        path: "to_read.book reading.book finished.book",
+        select: "authors thumbnail title"
+      })
+      .select("-password");
   }
 };
 
