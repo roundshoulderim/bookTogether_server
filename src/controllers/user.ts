bebbcf1fcd16a6c81c99431f5e947e56edb99f65
Andrew Chung <drew.c3071@gmail.com>
2019-11-30 18:17:30 +0900

    edit PATCH /user to account for changing pw

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response } from "express";
-import authorizationCheck from "../helpers/middleware/authorizationCheck";
+import express, { Request, Response, NextFunction } from "express";
 import upload from "../config/s3-upload";
 import userService from "../services/userService";
+import Unauthorized from "../helpers/errors/unauthorized";
 import InternalError from "../helpers/errors/internalError";
 
 const userRouter: express.Router = express.Router();
@@ -12,9 +12,7 @@ userRouter.get("/", async (req: Request, res: Response) => {
   if (!resetPasswordToken && !user) {
     const message: string =
       "인증을 한 후에만 사용자 정보를 불러올 수 있습니다.";
-    return res
-      .status(401)
-      .send({ error: { status: 401, type: "Unauthorized", message } });
+    return res.status(401).send(Unauthorized(message));
   }
   try {
     const getUserRes = await userService.getUser({ resetPasswordToken, user });
@@ -29,19 +27,31 @@ userRouter.get("/", async (req: Request, res: Response) => {
 
 userRouter.patch(
   "/",
-  authorizationCheck("인증을 한 후에만 사용자 정보를 업데이트 할 수 있습니다."),
+  (req: Request, res: Response, next: NextFunction) => {
+    if (!req.query.resetPasswordToken && !req.session.user) {
+      const message: string =
+        "인증을 한 후에만 사용자 정보를 업데이트할 수 있습니다.";
+      return res.status(401).send(Unauthorized(message));
+    }
+    next();
+  },
   upload.single("image"),
   async (req: Request, res: Response) => {
     if (req.file) {
       req.body.image = req.file.location;
     }
+    const patchBody: object = req.body;
     try {
-      const patchUserRes = await userService.patchUser(
-        req.session.user,
-        req.body
-      );
+      const patchUserRes = await userService.patchUser({
+        patchBody,
+        resetPasswordToken: req.query.resetPasswordToken,
+        user: req.session.user
+      });
       res.status(200).send(patchUserRes);
     } catch (error) {
+      if (error.status === 403 || error.status === 404) {
+        return res.status(error.status).send({ error });
+      }
       res.status(500).send(InternalError);
     }
   }
