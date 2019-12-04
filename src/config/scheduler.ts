7d329f91e251158cc2f31afd0c6daad9d4968f49
Andrew Chung <drew.c3071@gmail.com>
2019-12-04 16:49:31 +0900

    add comments for scheduled job time

--- src/config/scheduler.ts
+++ src/config/scheduler.ts
@@ -4,8 +4,9 @@ import migrateBooks from "./esmigration/migrateBooks";
 import migrateReviews from "./esmigration/migrateReviews";
 
 const scheduleMigrations = () => {
-  schedule.scheduleJob({ hour: 3, minute: 0, dayOfWeek: 0 }, kakaoApiToDB);
+  schedule.scheduleJob({ hour: 3, minute: 0, dayOfWeek: 0 }, kakaoApiToDB); // Sunday 3AM
   schedule.scheduleJob({ hour: 3, minute: 0, dayOfWeek: 3 }, () => {
+    // Wednesday 3AM
     migrateBooks();
     migrateReviews();
   });
